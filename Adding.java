fc0439c58491c3a2c5aa8aa7e487c58f7d815d61
Mohammed Inzamamul Haque <101422993+haqueinzamam@users.noreply.github.com>
2023-06-20 12:43:00 +0530

    Update and rename adding.java to Adding.java

---
+++ Adding.java
@@ -0,0 +1,10 @@
+public class Adding  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225; 
+int n2 = 115; 
+int sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+} 
